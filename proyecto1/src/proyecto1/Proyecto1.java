203817d02de9c6bfdd2b8e615ccf8a96e90b5bf4
ColsutecR <ColsutecR@ColsutecR4-PC>
2016-02-08 19:49:44 -0500

    primera version besida

---
+++ proyecto1/src/proyecto1/Proyecto1.java
@@ -0,0 +1,17 @@
+
+package proyecto1;
+
+/**
+ *
+ * @author ColsutecR
+ */
+public class Proyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
