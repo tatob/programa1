24b8e05d1f30e718967abb648b736e0fa18d3a8b
ColsutecR <ColsutecR@ColsutecR4-PC>
2016-02-08 19:59:30 -0500

    segunda vercion definicion de variables1.0

--- proyecto1/src/proyecto1/Proyecto1.java
+++ proyecto1/src/proyecto1/Proyecto1.java
@@ -1,17 +1,12 @@
 
 package proyecto1;
 
-/**
- *
- * @author ColsutecR
- */
 public class Proyecto1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        public static void main(String[] args) {
+         int a = 5+5;
+         int suma= a;
+         
     }
     
 }
