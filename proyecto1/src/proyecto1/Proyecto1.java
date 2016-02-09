a7e7a4adfa4c0c86971fcd3cea4b2d384deac8a7
ColsutecR <ColsutecR@ColsutecR4-PC>
2016-02-08 20:11:41 -0500

    coreccion tercera version 1.1

--- proyecto1/src/proyecto1/Proyecto1.java
+++ proyecto1/src/proyecto1/Proyecto1.java
@@ -4,11 +4,10 @@ package proyecto1;
 public class Proyecto1 {
 
         public static void main(String[] args) {
-         int a = 5+5;
-         int suma= a;
-            String la;
-            
-         System.out.prinlant("la suma es"+ la suma );
+         int a = 5+5 ;
+         int suma= a ;
+                       
+   System.out.println("la suma es: " + suma);
          
     }
     public void metodo(){
