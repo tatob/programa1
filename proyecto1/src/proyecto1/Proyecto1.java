b98dd550edda14abfb9a337efea900b3f3fdaa62
ColsutecR <ColsutecR@ColsutecR4-PC>
2016-02-08 20:06:38 -0500

    tercera  vercion metodo 1.1

--- proyecto1/src/proyecto1/Proyecto1.java
+++ proyecto1/src/proyecto1/Proyecto1.java
@@ -6,7 +6,12 @@ public class Proyecto1 {
         public static void main(String[] args) {
          int a = 5+5;
          int suma= a;
+            String la;
+            
+         System.out.prinlant("la suma es"+ la suma );
          
     }
-    
+    public void metodo(){
+        
+    }
 }
